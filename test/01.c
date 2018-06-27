f77df2625e95947799c2a1447b0e10de40925f52
Alper Akcan <alper.akcan@gmail.com>
2018-06-27 15:45:18 +0300

    float

--- test/01.c
+++ test/01.c
@@ -33,6 +33,8 @@ int main (int argc, char *argv[])
 	rc |= linearbuffers_output_uint16_set(encoder, 5);
 	rc |= linearbuffers_output_uint32_set(encoder, 6);
 	rc |= linearbuffers_output_uint64_set(encoder, 7);
+	rc |= linearbuffers_output_float_set(encoder, 0.8);
+	rc |= linearbuffers_output_double_set(encoder, 0.9);
 	rc |= linearbuffers_output_string_set(encoder, "1234567890");
 	rc |= linearbuffers_output_anum_set(encoder, linearbuffers_a_enum_1);
 	rc |= linearbuffers_output_end(encoder);
