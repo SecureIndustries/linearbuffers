a4f381e238cea9b8b3bdbfd370f4bd8319355b45
Alper Akcan <alper.akcan@gmail.com>
2019-05-21 19:22:41 +0300

    schema js generator

---
+++ src/schema-js.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#include "debug.h"
+#include "schema.h"
+#include "schema-private.h"
+
+int schema_generate_js_encoder (struct schema *schema, FILE *fp, int encoder_include_library)
+{
+        (void) schema;
+        (void) fp;
+        (void) encoder_include_library;
+        return 0;
+}
+
+int schema_generate_js_decoder (struct schema *schema, FILE *fp, int decoder_use_memcpy)
+{
+        (void) schema;
+        (void) fp;
+        (void) decoder_use_memcpy;
+        return 0;
+}
+
+int schema_generate_js_jsonify (struct schema *schema, FILE *fp, int decoder_use_memcpy)
+{
+        (void) schema;
+        (void) fp;
+        (void) decoder_use_memcpy;
+        return 0;
+}
