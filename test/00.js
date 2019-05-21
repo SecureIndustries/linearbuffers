5589036f77b6835a7e320d087757370477ce5b96
Alper Akcan <alper.akcan@gmail.com>
2019-05-21 14:52:10 +0300

    js

---
+++ test/00.js
@@ -0,0 +1,10 @@
+
+var LinearBuffers = require('../src/encoder.js');
+
+var encoder;
+
+encoder = new LinearBuffers.LinearBuffersEncoder();
+if (encoder == null) {
+	throw "can not create encoder";
+}
+delete encoder;
